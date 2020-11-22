43a3d4f503628b6f5478c92662e41736de953b84
Yathartha Sharma <yatharthasharma09@gmail.com>
2020-11-22 17:06:45 +0000

    Move from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Components/Header/Header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import "./App.css";
